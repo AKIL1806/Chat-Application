29df6e5a5e3cdb45aa28ad3b96e4c5d299c11751
AKIL1806 <akilchowdary696@gmail.com>
2025-04-18 17:06:45 +0530

    Updated back-end to store usernames with messages and broadcast them via WebSocket

--- back-end/index.js
+++ back-end/index.js
@@ -1,45 +1,41 @@
 const express = require('express');
 const redis = require('redis');
 const cors = require('cors');
-const { createServer } = require('http');
+const http = require('http');
 const { Server } = require('socket.io');
 
 const app = express();
-const httpServer = createServer(app);
-const io = new Server(httpServer, {
-  cors: {
-    origin: 'http://localhost:5173', // Adjust if different
-    methods: ['GET', 'POST']
-  }
-});
-
 app.use(cors());
 app.use(express.json());
+
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: '*',
+  },
+});
 
 const client = redis.createClient();
 client.connect().catch(console.error);
 
-// Fetch previous messages
 app.get('/messages', async (req, res) => {
   const messages = await client.lRange('chat_messages', 0, -1);
   res.json(messages.map(msg => JSON.parse(msg)));
 });
 
-// Store new messages
 app.post('/messages', async (req, res) => {
   const message = req.body;
   await client.rPush('chat_messages', JSON.stringify(message));
   res.status(201).send('Message stored');
 });
 
-// Handle WebSocket messages
 io.on('connection', (socket) => {
   socket.on('chat message', async (msg) => {
-    await client.rPush('chat_messages', JSON.stringify(msg)); // Save to Redis
-    io.emit('chat message', msg); // Broadcast to all clients
+    await client.rPush('chat_messages', JSON.stringify(msg));
+    io.emit('chat message', msg);
   });
 });
 
-httpServer.listen(3000, () => {
+server.listen(3000, () => {
   console.log('Server is running on http://localhost:3000');
 });
