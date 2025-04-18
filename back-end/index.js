9547eab1f52383f0d7c3d22c0de369ef02311fc4
AKIL1806 <akilchowdary696@gmail.com>
2025-04-18 17:15:58 +0530

    Updated back-end to persist and broadcast messages with usernames

--- back-end/index.js
+++ back-end/index.js
@@ -1,41 +1,48 @@
 const express = require('express');
 const redis = require('redis');
 const cors = require('cors');
-const http = require('http');
+const { createServer } = require('http');
 const { Server } = require('socket.io');
 
 const app = express();
-app.use(cors());
-app.use(express.json());
-
-const server = http.createServer(app);
-const io = new Server(server, {
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
   cors: {
-    origin: '*',
+    origin: 'http://localhost:5173', // Vite default port
+    methods: ['GET', 'POST'],
   },
 });
+
+app.use(cors());
+app.use(express.json());
 
 const client = redis.createClient();
 client.connect().catch(console.error);
 
+// API endpoint to fetch all messages
 app.get('/messages', async (req, res) => {
   const messages = await client.lRange('chat_messages', 0, -1);
   res.json(messages.map(msg => JSON.parse(msg)));
 });
 
+// API endpoint to post a new message
 app.post('/messages', async (req, res) => {
   const message = req.body;
   await client.rPush('chat_messages', JSON.stringify(message));
-  res.status(201).send('Message stored');
+  io.emit('chat message', message); // emit to all clients
+  res.status(201).send('Message stored and broadcasted');
 });
 
+// WebSocket connection
 io.on('connection', (socket) => {
+  console.log('New WebSocket connection');
+
   socket.on('chat message', async (msg) => {
     await client.rPush('chat_messages', JSON.stringify(msg));
     io.emit('chat message', msg);
   });
 });
 
-server.listen(3000, () => {
+httpServer.listen(3000, () => {
   console.log('Server is running on http://localhost:3000');
 });
