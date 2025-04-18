49eb1206e2945c7ffc047e56776805c45585a1f8
AKIL1806 <akilchowdary696@gmail.com>
2025-04-18 16:53:18 +0530

    Enhanced back-end to store and broadcast messages with username

--- back-end/index.js
+++ back-end/index.js
@@ -1,15 +1,15 @@
 const express = require('express');
-const http = require('http');
-const { Server } = require('socket.io');
 const redis = require('redis');
 const cors = require('cors');
+const { createServer } = require('http');
+const { Server } = require('socket.io');
 
 const app = express();
-const server = http.createServer(app);
-const io = new Server(server, {
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
   cors: {
-    origin: "http://localhost:5173", // your React front-end
-    methods: ["GET", "POST"]
+    origin: 'http://localhost:5173', // Adjust if different
+    methods: ['GET', 'POST']
   }
 });
 
@@ -19,35 +19,27 @@ app.use(express.json());
 const client = redis.createClient();
 client.connect().catch(console.error);
 
-// REST API endpoints
+// Fetch previous messages
 app.get('/messages', async (req, res) => {
   const messages = await client.lRange('chat_messages', 0, -1);
   res.json(messages.map(msg => JSON.parse(msg)));
 });
 
+// Store new messages
 app.post('/messages', async (req, res) => {
   const message = req.body;
   await client.rPush('chat_messages', JSON.stringify(message));
   res.status(201).send('Message stored');
 });
 
-// WebSocket setup
-io.on("connection", (socket) => {
-  console.log("A user connected");
-
-  socket.on("chat message", async (msg) => {
-    // Store in Redis
-    await client.rPush("chat_messages", JSON.stringify(msg));
-
-    // Broadcast to all clients
-    io.emit("chat message", msg);
-  });
-
-  socket.on("disconnect", () => {
-    console.log("A user disconnected");
+// Handle WebSocket messages
+io.on('connection', (socket) => {
+  socket.on('chat message', async (msg) => {
+    await client.rPush('chat_messages', JSON.stringify(msg)); // Save to Redis
+    io.emit('chat message', msg); // Broadcast to all clients
   });
 });
 
-server.listen(3000, () => {
+httpServer.listen(3000, () => {
   console.log('Server is running on http://localhost:3000');
 });
